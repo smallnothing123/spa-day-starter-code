5759ac2c6a48331c5907aa0d6e2465de09433bd4
Andrew <drmadness1234@gmail.com>
2023-05-08 20:00:52 -0500

    JOE JMFDOSAGNFDGMFDS

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,17 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank
+    @Size(min = 5, max = 15)
     private String username;
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 6)
     private String password;
 
     public User() {
